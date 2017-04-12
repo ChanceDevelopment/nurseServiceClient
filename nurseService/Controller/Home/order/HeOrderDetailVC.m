7a3dfafda16b6d1d90f0e4d35ca3d2bef6fe0a42
wukong <wukong.huaguoshan.com>
2017-04-12 20:28:14 +0800

    修改bug

--- nurseService/Controller/Home/order/HeOrderDetailVC.m
+++ nurseService/Controller/Home/order/HeOrderDetailVC.m
@@ -368,7 +368,7 @@
         sepLineX = CGRectGetMaxX(circleIcon.frame) + 2;
     }
     
-    sepLineW = SCREENWIDTH - sepLineX;
+    sepLineW = SCREENWIDTH +SCREENWIDTH;
     UIView *sepLine = [[UIView alloc] initWithFrame:CGRectMake(sepLineX, sepLineY, sepLineW, sepLineH)];
     sepLine.backgroundColor = [UIColor colorWithWhite:237.0 / 255.0 alpha:1.0];
     if (statusType == [statusArray count]) {
@@ -713,6 +713,7 @@
                     //30
                     CGFloat pointY = 0;
                     CGFloat pointX = 10;
+                    CGFloat pointx = 0;
                     UIImageView *headImageView = [[UIImageView alloc] initWithFrame:CGRectMake(pointX, pointY, 40, 40)];
                     headImageView.backgroundColor = [UIColor clearColor];
                     headImageView.layer.masksToBounds = YES;
@@ -754,16 +755,25 @@
                     [cell addSubview:hospitalL];
                     hospitalL.text = nurseWorkNnit;
                     
-                    pointY = 10;
+                    pointY = 0;
                     pointX = SCREENWIDTH-35;
-                    UIImageView *telephone = [[UIImageView alloc] initWithFrame:CGRectMake(pointX, pointY, 20, 20)];
+                    pointx = SCREENWIDTH-50;
+                    
+                    UIView *neww = [UIView new];
+                    neww.frame = CGRectMake(pointx, pointY, 50, 50);
+                    neww.backgroundColor = [UIColor clearColor];
+                    neww.userInteractionEnabled = YES;
+                    [cell addSubview:neww];
+                    
+                    UIImageView *telephone = [[UIImageView alloc] initWithFrame:CGRectMake(pointX, 10, 20, 20)];
                     telephone.backgroundColor = [UIColor clearColor];
                     telephone.image = [UIImage imageNamed:@"icon_phone"];
-                    telephone.userInteractionEnabled = YES;
+                    
                     [cell addSubview:telephone];
                     
+                    
                     UITapGestureRecognizer *userInfoTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(callCustomer)];
-                    [telephone addGestureRecognizer:userInfoTap];
+                    [neww addGestureRecognizer:userInfoTap];
                     break;
                 }
                 case 1:
@@ -1706,6 +1716,7 @@
 }
 
 - (void)callCustomer{
+    NSLog(@"点击了打电话");
     NSString *nursePhone = orderDetailDict[@"nursePhone"];
     if ([nursePhone isMemberOfClass:[NSNull class]] || nursePhone == nil) {
         [self showHint:@"暂无护士的联系方式"];
